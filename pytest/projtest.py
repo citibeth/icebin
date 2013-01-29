339cad6f169bc5ece2777ecbecc19c525439c8e8
Bob Fischer <rpf2116@columbia.edu>
2013-01-28 21:32:13 -0500

    Added test for correct pyProj library

---
+++ pytest/projtest.py
@@ -0,0 +1,17 @@
+import giss.proj
+import pyproj
+
+sproj = "+proj=stere +lon_0=-39 +lat_0=90 +lat_ts=71.0 +ellps=WGS84"
+
+xyproj = pyproj.Proj(sproj)
+llproj = pyproj.Proj(sproj)
+llproj.to_latlong()
+
+x=15.0
+y=15.0
+
+lon, lat = pyproj.transform(xyproj, llproj, x, y)
+#lon, lat = pyproj.transform(llproj, xyproj, x, y)
+
+print lon
+print lat
