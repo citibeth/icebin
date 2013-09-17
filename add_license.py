02a343811e91ccd9a9e077d4d41e4f1e1a1a5d2c
Bob Fischer <rpf2116@columbia.edu>
2013-09-16 22:07:10 -0400

    Fixed copyright notice

--- add_license.py
+++ add_license.py
@@ -1,9 +1,26 @@
+# GLINT2: A Coupling Library for Ice Models and GCMs
+# Copyright (c) 2013 by Robert Fischer
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import re
 import os.path
 import sys
 
 license = [
 "GLINT2: A Coupling Library for Ice Models and GCMs",
+"Copyright (c) 2013 by Robert Fischer",
 "",
 "This program is free software: you can redistribute it and/or modify",
 "it under the terms of the GNU General Public License as published by",
@@ -49,9 +66,9 @@ for line in license :
 	ret.append(' * ')
 	ret.append(line)
 	ret.append('\n')
-ret.append('*/\n')
+ret.append(' */\n')
 ret.append('\n')
-re_c = re.compile(r".*?\n * along with this program.  If not, see <http://www.gnu.org/licenses/>.\n*/(\n\n)", re.MULTILINE|re.DOTALL)
+re_c = re.compile(r".*?\n \* along with this program.  If not, see <http://www.gnu.org/licenses/>.\n \*/\n\n", re.MULTILINE|re.DOTALL)
 license_c = "".join(ret)
 licenses['.c'] = (license_c, re_c)
 licenses['.h'] = (license_c, re_c)
