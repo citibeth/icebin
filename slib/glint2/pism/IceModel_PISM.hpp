9e6ed0d266bc0dad721357cda71078ed3d9b5a6b
Bob Fischer <rpf2116@columbia.edu>
2014-02-07 12:52:26 -0500

    Undid accidental mangling

--- slib/glint2/pism/IceModel_PISM.hpp
+++ slib/glint2/pism/IceModel_PISM.hpp
@@ -28,8 +28,9 @@ namespace pism {
 // =================================================
 
 
-class IceModel_PISM
+class IceModel_PISM : public IceModel_Decode
 {
+	std::shared_ptr<glint2::Grid_XY const> glint2_grid;
 	MPI_Comm pism_comm;			// Commnicator used by ice model
 	PetscMPIInt pism_rank, pism_size;
 
@@ -41,7 +42,7 @@ class IceModel_PISM
 	std::unique_ptr<::PISMConfig> config;
 	std::unique_ptr<::PISMConfig> overrides;
 	std::unique_ptr<::IceGrid> pism_grid;
-	std::unique_ptr<::PISMIceModel> ice_model;
+	std::unique_ptr<PISMIceModel> ice_model;   // No "::"
 	PSConstantGLINT2 *pism_surface_model;	// We don't own this.
 
 	// Stuff used for Scatter/Gather
@@ -53,6 +54,16 @@ class IceModel_PISM
 	// Temporary stuff to hold returns from PISM
 	IceModelVec2S strain_heating2;		//!< ice surface elevation; ghosted
 
+
+
+	// Corresponding PISM variable for each field
+	std::map<IceField, IceModelVec2S *> pism_vars;
+
+	double BY_ICE_DENSITY;		// CONSTANT Used to prepare input for PISM
+
+	/** Use a DISMAL ice model to save stuff easily (for debugging) */
+	std::unique_ptr<IceModel_DISMAL> dismal;
+
 	// ------------------------
 public:
 	/** Not sure if this is used... */
@@ -61,6 +72,8 @@ public:
 	/** Initialize any grid information, etc. from the IceSheet struct.
 	@param vname_base Construct variable name from this, out of which to pull parameters from netCDF */
 	void init(
+		IceModel::GCMParams const &gcm_params,
+		std::shared_ptr<glint2::Grid> const &grid2,
 		NcFile &nc,
 		std::string const &vname_base,
 		NcVar *const_var);
@@ -69,10 +82,46 @@ public:
 	int ny() { return pism_grid->My; }
 
 	// ----------------------------------------------
+	/** Converts ij indices to PISM
+	@param i Index in x ("horizontal") direction
+	@param j Index in y ("vertical") direction, toward the poles */
+	long ij_to_pism1d(int i, int j) const
+		{ return i * glint2_grid->ny() + j; }
+
+	void pism1d_to_ij(long ix1, int &i, int &j)
+	{
+		i = ix1 / ny();
+		j = ix1 - (i * ny());
+	}
+
+	// ----------------------------------------------
+	long pism1d_to_glint2(long ix_pism)
+	{
+		int i, j;
+		pism1d_to_ij(ix_pism, i, j);
+		return glint2_grid->ij_to_index(i, j);
+	}
+
+	long glint2_to_pism1d(long ix_glint2)
+	{
+		int i, j;
+		glint2_grid->index_to_ij(ix_glint2, i, j);
+		return ij_to_pism1d(i, j);
+	}
+
+	// ----------------------------------------------
 	PetscErrorCode iceModelVec2S_to_blitz_xy(IceModelVec2S &pism_var, blitz::Array<double,2> &ret);
 	// ----------------------------------------------
 
+	void update_ice_sheet(
+		NcFile &nc,
+		std::string const &vname,
+		IceSheet *sheet);
+
 	~IceModel_PISM();
+
+	/** Query all the ice models to figure out what fields they need */
+	void get_required_fields(std::set<IceField> &fields);
 
 	PetscErrorCode allocate(
 		std::shared_ptr<const glint2::Grid_XY> &,
@@ -80,10 +129,26 @@ public:
 
 	PetscErrorCode deallocate();
 
-protected:
+#if 0
+public:
+	/** Inherited from IceModel */
+	void run_timestep(double time_s,
+		blitz::Array<int,1> const &indices,
+		std::map<IceField, blitz::Array<double,1>> const &vals2);
+private:
+	PetscErrorCode run_timestep_petsc(double time_s,
+		blitz::Array<int,1> const &indices,
+		std::map<IceField, blitz::Array<double,1>> const &vals2);
+
+#else
+public:
+	void run_decoded(double time_s,
+		std::map<IceField, blitz::Array<double,1>> const &vals2);
+private:
 	PetscErrorCode run_decoded_petsc(double time_s,
 		std::map<IceField, blitz::Array<double,1>> const &vals2);
 
+#endif
 
 };
 
