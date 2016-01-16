2e780b55b42e15d503e288f0cb91ca3ad1dbaf95
citibeth <rpf2116@columbia.edu>
2016-01-15 22:23:36 -0500

    Cython problems fixed.

--- pyext/_icebin.pyx
+++ pyext/_icebin.pyx
@@ -1,22 +1,21 @@
 
 # This will make the C++ class def for Rectangle available..
-cdef extern from "icebin/Grid.hpp" namesapce "icebin":
+cdef extern from "icebin/Grid.hpp" namespace "icebin":
 
 	# Now, let’s add the Rectangle class to this extern from block -
     # just copy the class name from Rectangle.h and adjust for Cython
     # syntax, so now it becomes:
 	cdef cppclass Vertex:
 		# We now need to declare the attributes and methods for use on Cython:
-		cdef cppclass Vertex:
-			Vertex(double, double, int) except +
-			long index
-			double const x
-			double const y
+		Vertex(double, double, int) except +
+		long index
+		const double x
+		const double y
 
 cdef class PyVertex:
 	cdef Vertex *thisptr
 	def __cinit__(self, double x, double y, int index):
-		self.thisptr = new Cell(x,y,index)
+		self.thisptr = new Vertex(x,y,index)
 	def __dealloc__(self):
 		del self.thisptr
 
