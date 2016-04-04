7b97f12a5390ae4c4001626f295422a52296d6e7
citibeth <rpf2116@columbia.edu>
2016-04-03 23:26:44 -0400

    Line integrals of functions on finite element meshes.

---
+++ pylib/icebin/element_l1.py
@@ -0,0 +1,144 @@
+# IceBin: A Coupling Library for Ice Models and GCMs
+# Copyright (c) 2013-2016 by Elizabeth Fischer
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import numpy as np
+import scipy.sparse
+
+"""Computes a regridding matrix between an L1 (piecewise linear)
+finite elements and a L0 ("regular") grid with constant-value grid
+cells.
+
+These functions are meant to work with ibgrid"""
+
+
+def eqn_plane_subelement(element, basis_vertex):
+    """Provides the equation for the sub-element with vertices at three points:
+        element: (p0, p1, p2) = ((p0x,p0y), (p1x,p1y), (p2x,p2y))
+    Equation is z = Ax+By+C, returned as (A,B,C)
+
+    If basis_vertex == 0, we solve for A,B,C in:
+        Ap0x + Bp0y + C = p0z
+        Ap1x + Bp1y + C = 0
+        AP2x + Bp2y + C = 0
+
+    basis_vertex: Index (0-2) of the vertex that is set to 1 for this
+    sub-basis function; other vertices equal 0.
+    """
+
+    M = np.zeros((3,3))
+    M[0,0] = element[0].x
+    M[0,1] = element[0].y
+    M[1,0] = element[1].x
+    M[1,1] = element[1].y
+    M[2,0] = element[2].x
+    M[2,1] = element[2].y
+    M[:,2] = 1.
+    rhs = np.zeros((3,))
+    rhs[basis_vertex] = 1.      # p0z
+    return np.linalg.solve(M, rhs)
+
+
+
+def integrate_subelement(element, basis_vertex, polygon):
+    """Integrates a sub-element's plane around a polygon.
+    A sub-element is defined as the plane z=Ax+By+C defined by one
+        vertex in one element.
+
+    element: [p0, p1, p2] list(Vertex)
+        The points in the element's triangle.
+    polygon: [q0, q1, ...]
+        The polygon to integrate around.  It will often be the same as
+        element, but will sometimes be smaller."""
+
+    A,B,C = eqn_plane_subelement(element, basis_vertex)
+    # print('ABC', element, A,B,C)
+    total = 0.
+    q0=polygon[-1]
+    for q1 in polygon:
+        # These formulae were obtained from element3.maxima.
+        # Run:   maxima <element3.maxima
+        A_coeff = (1./6.)  *  (0 \
+            + 2 * q0.x * q0.x * q0.y \
+            +   q0.x * q0.x * q1.y \
+            -   q0.x * q0.y * q1.x \
+            +   q0.x * q1.x * q1.y \
+            -   q0.y * q1.x * q1.x \
+            - 2 * q1.x * q1.x * q1.y )
+
+        B_coeff = (1./6.)  *  (0 \
+            - 2  * q0.x * q0.y * q0.y \
+            +   q0.x * q0.y * q1.y \
+            -   q0.y * q0.y * q1.x \
+            +   q0.x * q1.y * q1.y \
+            -   q0.y * q1.x * q1.y \
+            + 2 * q1.x * q1.y * q1.y)
+
+        C_coeff = .5  *  (q0.x * q1.y - q0.y * q1.x)
+
+        total += A*A_coeff + B*B_coeff + C*C_coeff
+        q0 = q1
+    return total
+
+
+def compute_AvI(gridX, gridI):
+    """Computes A<-I regridding matrix.
+    returns: AvI, weightsA, weightsI
+
+    To regrid using this matrix:
+        fA = (1/weightsA) AvI fI
+        fI = (1/weightsI) IvA fA    where IvA = transpose(AvI)
+
+    NOTE: weightsA is the area of A that OVERLAPS I.  Similar for
+        weightsI.  If you wish to be weighting by the FULL area of A,
+        compute that elsewhere.
+
+    Returns: AvI, weightsA, weightsI"""
+
+
+    # Iterate through polygons in exchange grid.  This is the efficient
+    # way to find ice basis functions relevant to teach atmosphere grid
+    # cell.  Each of these polygons might involve multiple ice basis
+    # functions
+    data = []
+    ii = []
+    jj = []
+    for cellX in gridX.cells.values():
+        # Get the polygon we wish to integrate around
+        polygon = cellX.vertices
+        ix_A = cellX.i      # Index in the atmosphere grid
+
+        # Fish out the element definition from gridI
+        element = gridI.cells[cellX.j].vertices
+
+        # Loop over the partial basis functions for this element
+        # Each partial basis function is 1 at one vertex and 0 at the other two
+        for basis_vertex in range(0,len(element)):
+            # Index of this basis function in the ice mesh
+            ix_I = element[basis_vertex].index
+
+            val = integrate_subelement(element, basis_vertex, polygon)
+
+            #AvI.add((ix_A, ix_I), val) # Equiv. to AvI[poly_iA, poly_iI] += val
+            data.append(val)
+            ii.append(ix_A)
+            jj.append(ix_I)
+
+    AvI = scipy.sparse.coo_matrix((data,(ii,jj)),
+        shape=(gridX.grid1_ncells_full, gridX.grid2_nvertices_full))
+
+    weightsA = np.asarray(AvI.sum(1)).transpose()[0]
+    weightsI = np.asarray(AvI.sum(0))[0]
+    return (AvI, weightsA, weightsI)
---
+++ pylib/icebin/tests/test_regrid_l1.py
@@ -0,0 +1,164 @@
+# IceBin: A Coupling Library for Ice Models and GCMs
+# Copyright (c) 2013-2016 by Elizabeth Fischer
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from icebin import ibgrid, element_l1
+from matplotlib import pyplot
+import giss.basemap
+import unittest
+import numpy as np
+
+projection='+proj=stere +lon_0=-39 +lat_0=90 +lat_ts=71.0 +ellps=WGS84'
+class TestMesh(unittest.TestCase):
+
+    def setUp(self):
+        # -------------- Ice Grid
+        vertices = [ibgrid.Vertex(spec[0], spec[1], spec[2]) for spec in [
+            (0, -1., -1.),
+            (1,  1., -1.),
+            (2,  1.,  1.),
+            (3, -1.,  1.),
+            (4,  0.,  0.)]]
+
+        vx = vertices
+        cells = [ibgrid.Cell(*spec) for spec in [
+            (0, (vx[0], vx[1], vx[4]), 0,0,0,0),
+            (1, (vx[1], vx[2], vx[4]), 0,0,0,0),
+            (2, (vx[2], vx[3], vx[4]), 0,0,0,0),
+            (3, (vx[3], vx[0], vx[4]), 0,0,0,0)]]
+
+        self.gridI = ibgrid.Grid(vertices, cells,
+            projection=projection,
+            type='MESH',
+            coordinates='XY')
+
+    def test_A1(self):
+        """A has a single grid cell covering everything."""
+
+        # ------- Ice Grid
+        gridI = self.gridI
+
+        # ------- Atmosphere Grid
+        vertices = [ibgrid.Vertex(spec[0], spec[1], spec[2]) for spec in [
+            (0, -2., -2.),
+            (1,  2., -2.),
+            (2,  2.,  2.),
+            (3, -2.,  2.)]]
+        vx = vertices
+        cells = [ibgrid.Cell(0, [vx[0], vx[1], vx[2], vx[3]], 0,0,0,0)]
+
+        gridA = ibgrid.Grid(vertices, cells,
+            projection=projection,
+            type='MESH',
+            coordinates='XY')
+
+        # ------ Exchange Grid
+        vertices = dict(gridI.vertices)
+        vx = vertices
+        cells = [ibgrid.Cell(*spec) for spec in [
+            (0, (vx[0], vx[1], vx[4]), 0,0,0,0),
+            (1, (vx[1], vx[2], vx[4]), 0,1,0,0),
+            (2, (vx[2], vx[3], vx[4]), 0,2,0,0),
+            (3, (vx[3], vx[0], vx[4]), 0,3,0,0)]]
+                
+        gridX = ibgrid.Grid(vx, cells,
+            projection=projection,
+            type='MESH',
+            coordinates='XY',
+            grid1_ncells_full=gridA.cells_num_full,
+            grid2_ncells_full=gridI.cells_num_full,
+            grid1_nvertices_full=gridA.vertices_num_full,
+            grid2_nvertices_full=gridI.vertices_num_full)
+
+        AvI,weightsA,weightsI = element_l1.compute_AvI(gridX, gridI)
+        print(AvI.todense())
+        print(weightsA)
+        print(weightsI)
+
+        self.assertAlmostEqual(2./3., weightsI[0])
+        self.assertAlmostEqual(2./3., weightsI[1])
+        self.assertAlmostEqual(2./3., weightsI[2])
+        self.assertAlmostEqual(2./3., weightsI[3])
+        self.assertAlmostEqual(1.+1./3., weightsI[4])
+
+        M = np.asarray(AvI.todense())
+        self.assertAlmostEqual(M[0,0], weightsI[0])
+        self.assertAlmostEqual(M[0,1], weightsI[1])
+        self.assertAlmostEqual(M[0,2], weightsI[2])
+        self.assertAlmostEqual(M[0,3], weightsI[3])
+        self.assertAlmostEqual(M[0,4], weightsI[4])
+
+        self.assertAlmostEqual(sum(weightsA), sum(weightsI))
+
+
+    def test_A2(self):
+        """A has a two grid cells."""
+
+        # ------- Ice Grid
+        gridI = self.gridI
+
+        # ------- Atmosphere Grid
+        vertices = [ibgrid.Vertex(spec[0], spec[1], spec[2]) for spec in [
+            (0, -2., -2.),
+            (1,  0., -2.),
+            (2,  2., -2.),
+            (3,  2.,  2.),
+            (4,  0.,  2.),
+            (5, -2.,  2.),]]
+        vx = vertices
+        cells = [ibgrid.Cell(*spec) for spec in [
+            (0, (vx[0], vx[1], vx[4], vx[5]), 0,0,0,0),
+            (1, (vx[1], vx[2], vx[3], vx[4]), 0,0,0,0)]]
+
+        gridA = ibgrid.Grid(vertices, cells,
+            projection=projection,
+            type='MESH',
+            coordinates='XY')
+
+        # ------ Exchange Grid
+        vertices = [ibgrid.Vertex(spec[0], spec[1], spec[2]) for spec in [
+            (0, -1., -1.),      # Original points from gridI
+            (1,  1., -1.),
+            (2,  1.,  1.),
+            (3, -1.,  1.),
+            (4,  0.,  0.),      # Middle point
+            (5,  0., -1.),      # Split points created by overlap
+            (6,  0.,  1.)]]
+
+        vx = vertices
+        cells = [ibgrid.Cell(*spec) for spec in [
+            (0, (vx[0], vx[5], vx[4]), 0,0,0,0),
+            (1, (vx[5], vx[1], vx[4]), 1,0,0,0),
+            (2, (vx[1], vx[2], vx[4]), 1,1,0,0),
+            (3, (vx[2], vx[6], vx[4]), 1,2,0,0),
+            (4, (vx[6], vx[3], vx[4]), 0,2,0,0),
+            (5, (vx[3], vx[0], vx[4]), 0,3,0,0)]]
+
+        gridX = ibgrid.Grid(vx, cells,
+            projection=projection,
+            type='MESH',
+            coordinates='XY',
+            grid1_ncells_full=gridA.cells_num_full,
+            grid2_ncells_full=gridI.cells_num_full,
+            grid1_nvertices_full=gridA.vertices_num_full,
+            grid2_nvertices_full=gridI.vertices_num_full)
+
+        AvI,weightsA,weightsI = element_l1.compute_AvI(gridX, gridI)
+        #print(AvI.todense())
+        #print(weightsA)
+        #print(weightsI)
+
+        self.assertAlmostEqual(sum(weightsA), sum(weightsI))
+
