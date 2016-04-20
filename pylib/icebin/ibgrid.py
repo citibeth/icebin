4fb5e991bc711d8c526805d54b8583b8054774d9
Elizabeth F <rpf2116@columbia.edu>
2016-04-19 23:15:53 -0400

    Ported functionality of C++ Indexing class to Python.  Needed to generate elevation-classified input files for ModelE.

--- pylib/icebin/ibgrid.py
+++ pylib/icebin/ibgrid.py
@@ -25,26 +25,18 @@ import operator
 # -------------------------------------------------------
 class Indexing(object):
     def __init__(self, nc, vname):
-        print('vname', vname)
         ncvar = nc.variables[vname]
         self.base = ncvar.base
         self.extent = ncvar.extent
         self.indices = ncvar.indices
 
-        print('w-base', self.base, type(self.base))
-        print('w-extent', self.extent)
-        print('w-indices', self.indices)
-
+        # Turn it all into Numpy arrays
         if not isinstance(self.base, np.ndarray):
             self.base = np.array([self.base], dtype=type(self.base))
         if not isinstance(self.extent, np.ndarray):
             self.extent = np.array([self.extent], dtype=type(self.extent))
         if not isinstance(self.indices, np.ndarray):
             self.indices = np.array([self.indices], dtype=type(self.indices))
-
-        print('x-base', self.base)
-        print('x-extent', self.extent)
-        print('x-indices', self.indices)
 
         self.size = 1
         for ex in self.extent:
@@ -57,8 +49,41 @@ class Indexing(object):
         else:
             self.shape = (self.extent[1], self.extent[0])
 
+        self.make_strides()
+
     def __len__(self):
         return functools.reduce(operator.mul, self.extent)
+
+
+    @property
+    def rank(self):
+        return len(self.extent)
+
+    def make_strides(self):
+        rank = self.rank
+        self.strides = np.zeros((rank,),dtype='i')
+        self.strides[self.indices[rank-1]] = 1;
+        for d in range(rank-2,-1,-1):
+            self.strides[self.indices[d]] = self.strides[self.indices[d+1]] * self.extent[self.indices[d+1]];
+
+    def tuple_to_index(self, tuple):
+        ix = 0
+        for k in range(0,self.rank):
+            ix += (tuple[k]-self.base[k]) * self.strides[k];
+        return ix;
+
+    def index_to_tuple(self, ix):
+        ix0 = ix
+        tpl = np.zeros(len(self.shape),dtype='i')
+        for d in range(0,self.rank-1):       # indices by descending stride
+            k = self.indices[d]
+            tpl_k = ix // self.strides[k]
+            ix -= tpl_k*self.strides[k]
+            tpl[k] = tpl_k + self.base[k]
+
+        tpl[self.indices[self.rank-1]] = ix
+        return tuple(tpl)
+
 
 # -------------------------------------------------------
 class Vertex(object):
@@ -168,7 +193,6 @@ class Grid(object):
         # Get unique line segments
         segments = set()
         for cell in cells:
-#            print('vertices', cell.vertices)
             v0 = cell.vertices[-1]
             for v1 in cell.vertices:
                 segments.add((v0,v1))
@@ -199,7 +223,6 @@ class Grid(object):
         ydata = np.array(ydata)
 
         if self.xyproj is not None :    # translate xy->ll
-            print('******** pyproj', type(self.xyproj), type(self.llproj))
             londata, latdata = pyproj.transform(self.xyproj, self.llproj, xdata, ydata)
             londata[np.isnan(xdata)] = np.nan
             latdata[np.isnan(ydata)] = np.nan
@@ -282,8 +305,6 @@ def read_nc(nc, vname):
 
         # Convert vertices to a list in the correct order
         vertices_as_list = [vertices[vertex_index] for vertex_index in vertex_indices]
-        if len(vertices_as_list) == 0:
-            print('cell vertices', vertices_as_list, r0, r1)
         cells[index] = Cell(index, vertices_as_list, ijk[0], ijk[1], ijk[2], area)
 
     if attrs['type'] == 'XY':
