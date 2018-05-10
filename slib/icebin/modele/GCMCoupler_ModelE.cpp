91915ac714ac59faaeed0e5b27f8aaa0e5768515
Elizabeth Fischer <elizabeth.fischer@columbia.edu>
2018-05-10 11:42:46 -0400

    Update for boost 1.68

--- slib/icebin/modele/GCMCoupler_ModelE.cpp
+++ slib/icebin/modele/GCMCoupler_ModelE.cpp
@@ -45,6 +45,7 @@
 #include <boost/archive/binary_iarchive.hpp>
 
 #include <boost/serialization/serialization.hpp>
+#include <boost/serialization/vector.hpp>
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/serialization/export.hpp>
 
